49879c17a089b124eedce6694ee8d8603a23853e
edwardstark <edwardowiejohn2005@gmail.com>
2026-06-22 11:29:26 +0100

    ping-pong

---
+++ ping-pong.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "pong")
+}
+
+func main() {
+	http.HandleFunc("/ping", pingHandler)
+
+	fmt.Println("Server is starting on port 8080...")
+
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
